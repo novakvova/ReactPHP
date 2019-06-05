b1f4e6d26b99bcbe3693005ea18408156e9b6c4b
novakvova <novakvova@gmail.com>
2019-06-05 18:18:00 +0300

    BaseAppRoter

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello Peter</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
