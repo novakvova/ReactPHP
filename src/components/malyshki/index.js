3e294a79cdcc1e9ca6b31964329e5e8388fefe77
valera <shvvrv@gmail.com>
2019-06-18 13:38:04 +0300

    Get data server by react

--- src/components/malyshki/index.js
+++ src/components/malyshki/index.js
@@ -8,6 +8,10 @@ class MalyshkiWidgetContainer extends Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount=() => {
+        //this.props.dispatch({type:'sobaka_event_toRedux'});
+        this.props.getListData();
+    }
     
     render() { 
         console.log('====Malyski props=====', this.props);
@@ -17,7 +21,22 @@ class MalyshkiWidgetContainer extends Component {
          );
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        list: get(state, 'malyshki.list.data'),
+        isListLoading: get(state, 'malyshki.list.loading'),
+        isListError: get(state, 'malyshki.list.error')
+    }
+}
 
-const MalyshkiWidget = connect(null)(MalyshkiWidgetContainer);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getListData: () => {
+            dispatch(malyshkiActions.getListData());
+        }
+    }
+}
+
+const MalyshkiWidget = connect(mapStateToProps, mapDispatchToProps)(MalyshkiWidgetContainer);
  
 export default MalyshkiWidget;
