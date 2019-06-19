bef463516a1e19ee78e38638a88e701913a8d166
novakvova <novakvova@gmail.com>
2019-06-19 12:23:35 +0300

    Begin works good

--- src/components/malyshki/index.js
+++ src/components/malyshki/index.js
@@ -14,9 +14,34 @@ class MalyshkiWidgetContainer extends Component {
     }
     
     render() { 
+
         console.log('====Malyski props=====', this.props);
+        const listContent = this.props.list.map(item => {
+            return (
+            <div key={item.id} className="col-lg-3 col-md-4 col-6">
+                <a href="#" className="d-block mb-4 h-100">
+                    <img className="img-fluid img-thumbnail" src={item.name} alt="" />
+                </a>
+            </div>
+            )
+        });
         return ( 
-            <h1>Список дівчат</h1>
+            <div>
+                
+                <h1>Список дівчат</h1>
+                <div className="container">
+
+                    <h1 className="font-weight-light text-center text-lg-left mt-4 mb-0">Thumbnail Gallery</h1>
+
+                    <hr className="mt-2 mb-5" />
+
+                    <div className="row text-center text-lg-left">
+                        
+                       {listContent}
+                    </div>
+
+                </div>
+            </div>
 
          );
     }
